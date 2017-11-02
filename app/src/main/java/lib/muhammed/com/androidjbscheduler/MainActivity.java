4728e64ba2b853e84f81d70db660476da7c07c7a
muhammed <muhammed.thasneem@yahoo.com>
2017-11-02 11:14:58 +0530

    Initial commit

---
+++ app/src/main/java/lib/muhammed/com/androidjbscheduler/MainActivity.java
@@ -0,0 +1,13 @@
+package lib.muhammed.com.androidjbscheduler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
